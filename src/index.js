b4aaec248c084d3c0fcaa90821379e6fd9967e73
Mike <miketu926@gmail.com>
2019-11-18 10:42:00 -0500

    cleanup and restructured files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'normalize.css';
-import MainTiles from './components/MainTiles.jsx'
+import MainTiles from './components/MainTiles/MainTiles'
 
 ReactDOM.render(<MainTiles />, document.getElementById('root'));
